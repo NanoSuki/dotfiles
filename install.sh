422369f407eafd9143a61b2b3dacfe2995f67862
Ushio <git@ushio.uk>
2025-11-08 00:04:19 +0800

    niri & ghostty: update config

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# You need to install clipse and noctalia-shell by yourself
+# You need to install clipse, noctalia-shell and maple font by yourself
 pacman -S niri xdg-desktop-portal-gnome gnome-keyring xwayland-satellite
 pacman -S hypridle xdg-desktop-portal-gtk xdg-user-dirs pipewire wireplumber polkit-kde-agent
 pacman -S libnotify ghostty network-manager-applet wl-clipboard
