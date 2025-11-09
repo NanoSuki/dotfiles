c1cef0bade81f7c9202b15c36fe288554261453b
Ushio <git@ushio.uk>
2025-11-09 18:45:29 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # You need to install clipse, noctalia-shell and maple font by yourself
-pacman -S niri xdg-desktop-portal-gnome gnome-keyring xwayland-satellite
-pacman -S hypridle xdg-desktop-portal-gtk xdg-user-dirs pipewire wireplumber polkit-kde-agent
-pacman -S libnotify ghostty network-manager-applet wl-clipboard
-pacman -S brightnessctl udiskie playerctl helvum pipewire-audio pipewire-alsa pipewire-pulse
-pacman -S nwg-look orchis-theme xcursor-comix
-pacman -S qt5-wayland qt6-wayland
+pacman -S --asexplicit niri xdg-desktop-portal-gnome gnome-keyring xwayland-satellite
+pacman -S --asexplicit hypridle xdg-desktop-portal-gtk xdg-user-dirs pipewire wireplumber polkit-kde-agent
+pacman -S --asexplicit libnotify ghostty network-manager-applet wl-clipboard
+pacman -S --asexplicit brightnessctl udiskie playerctl helvum pipewire-audio pipewire-alsa pipewire-pulse
+pacman -S --asexplicit nwg-look orchis-theme xcursor-comix
+pacman -S --asexplicit qt5-wayland qt6-wayland
 # Fonts
-pacman -S ttf-cascadia-code-nerd ttf-sarasa-gothic noto-fonts-emoji tela-circle-icon-theme-standard
+pacman -S --asexplicit ttf-cascadia-code-nerd ttf-sarasa-gothic noto-fonts-emoji tela-circle-icon-theme-standard
 # Noctalia-shell opt dep
-pacman -S cava wlsunset
+pacman -S --asexplicit cava wlsunset
