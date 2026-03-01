db2645d62210b59a4dbf46d4e24606c346e8974b
Ushio <git@ushio.uk>
2026-03-01 12:15:33 +0800

    install: do not use the --asexplicit flag

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-pacman -S --asexplicit niri xdg-desktop-portal-gnome gnome-keyring xwayland-satellite
-pacman -S --asexplicit xdg-desktop-portal-gtk pipewire wireplumber polkit-kde-agent
-pacman -S --asexplicit libnotify ghostty network-manager-applet
-pacman -S --asexplicit brightnessctl udiskie gammastep helvum pipewire-audio pipewire-alsa pipewire-pulse
-pacman -S --asexplicit nwg-look
-pacman -S --asexplicit qt5-wayland qt6-wayland
+pacman -S niri xdg-desktop-portal-gnome gnome-keyring xwayland-satellite
+pacman -S xdg-desktop-portal-gtk pipewire wireplumber polkit-kde-agent
+pacman -S libnotify ghostty network-manager-applet
+pacman -S brightnessctl udiskie gammastep helvum pipewire-audio pipewire-alsa pipewire-pulse
+pacman -S nwg-look
+pacman -S qt5-wayland qt6-wayland
 
 # Fonts
-pacman -S --asexplicit ttf-cascadia-code-nerd ttf-sarasa-gothic noto-fonts-emoji tela-circle-icon-theme-standard
+pacman -S ttf-cascadia-code-nerd ttf-sarasa-gothic noto-fonts-emoji tela-circle-icon-theme-standard
 
 # AUR deps:
 # clipse noctalia-shell ttf-maplemono-nf-cn catppuccin-cursors-latte
 
 # Noctalia-shell opt dep
-pacman -S --asexplicit cava power-profiles-daemon adw-gtk-theme
+pacman -S cava power-profiles-daemon adw-gtk-theme
