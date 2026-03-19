83511a1da88e35844e4b88c4d18516b8061d1dc4
Ushio <git@ushio.uk>
2026-03-19 23:48:30 +0800

    replace clipse with copyq

--- apply.sh
+++ apply.sh
@@ -4,7 +4,3 @@ rclone sync ghostty/ ~/.config/ghostty/ --verbose
 rclone sync niri/ ~/.config/niri/ --verbose
 rclone sync xdg-desktop-portal/ ~/.config/xdg-desktop-portal/ --verbose
 rclone sync gammastep/ ~/.config/gammastep/ --verbose
-
-# Add execution permissions
-cd $HOME/.config/niri/scripts
-chmod 744 *.sh
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 pacman -S niri xdg-desktop-portal-gnome gnome-keyring xwayland-satellite
 pacman -S xdg-desktop-portal-gtk pipewire wireplumber polkit-kde-agent
-pacman -S libnotify ghostty network-manager-applet
+pacman -S libnotify ghostty network-manager-applet copyq
 pacman -S brightnessctl udiskie gammastep helvum pipewire-audio pipewire-alsa pipewire-pulse
 pacman -S nwg-look
 pacman -S qt5-wayland qt6-wayland
@@ -10,7 +10,7 @@ pacman -S qt5-wayland qt6-wayland
 pacman -S ttf-cascadia-code-nerd ttf-sarasa-gothic noto-fonts-emoji tela-circle-icon-theme-standard
 
 # AUR deps:
-# clipse noctalia-shell ttf-maplemono-nf-cn catppuccin-cursors-latte
+# noctalia-shell ttf-maplemono-nf-cn catppuccin-cursors-latte
 
 # Noctalia-shell opt dep
 pacman -S power-profiles-daemon adw-gtk-theme
